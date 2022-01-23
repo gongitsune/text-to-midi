b7abb1b434771f9411fbbe6577a3b4c1fbb84db6
gongitsune <89068705+gongitsune@users.noreply.github.com>
2022-01-23 09:02:39 +0900

    Add molding function

--- src/midi.py
+++ src/midi.py
@@ -1,14 +1,15 @@
 from __future__ import annotations
-import image
-import pretty_midi as mid
-from PIL import Image
-from PIL import ImageOps
-import util
-import markov
-import json
+
 import itertools
+import json
 import random
 
+import pretty_midi as mid
+from PIL import Image, ImageOps
+
+import image
+import markov
+import util
 from music_chords import Chord
 
 
@@ -91,10 +92,13 @@ class ImageToMidi:
     def get_track(self) -> MyTrack:
         return self.track
 
-    def molding_to_chords(self, chord_order: list[str], bar_num: int = 8) -> MyTrack:
-        cur_index = 0
+    def molding_to_chords(
+        self, chord_order: list[str], bar_num: int = 8
+    ) -> ImageToMidi:
         keys = list(self.track.messages.keys())
-        for t_list in [keys[i : i + bar_num] for i in range(0, len(keys), bar_num)]:
+        for cur_index, t_list in enumerate(
+            [keys[i : i + bar_num] for i in range(0, len(keys), bar_num)]
+        ):
             note_nums = Chord(chord_order[cur_index % len(chord_order)]).note_numbers()
             for t in t_list:
                 use_note_num = []
@@ -122,8 +126,73 @@ class ImageToMidi:
                             t + self.duration,
                         )
                     )
-            cur_index += 1
-        return self.track
+        return self
+
+    def molding_volume(self) -> ImageToMidi:
+        for msgs in self.track.messages.values():
+            for msg in random.sample(msgs, int(len(msgs) / 2)):
+                msg.velocity = random.randint(40, 120)
+
+        return self
+
+    def change_duration(self, pos: float, duration: float, offset: float = 0) -> None:
+        for msg in self.track.messages[pos]:
+            msg.end = msg.start + duration
+        diff = None
+        for key, msgs in self.track.messages.items():
+            if key <= pos:
+                continue
+            if not diff:
+                diff = msgs[0].start - self.track.messages[pos][0].end
+            for msg in msgs:
+                msg.start = msg.start - diff + offset
+                msg.end = msg.end - diff + offset
+
+    def molding_duration(self) -> ImageToMidi:
+        durations = {self.duration: 1, self.duration * 0.5: 1}
+        state = {self.duration: 1, self.duration / 2: 1, self.duration * 1.5: 0.5}
+        current_state = [list(state.keys())[0], 0]
+        for key in self.track.messages.keys():
+            if random.randint(0, 1) != 1:
+                continue
+            if random.random() < current_state[1] / 10:
+                current_state = [
+                    random.choices(
+                        list(state.keys()), weights=list(state.values()), k=1
+                    )[0],
+                    0,
+                ]
+
+            current_state[1] += 1
+            duration = (
+                random.choices(
+                    list(durations.keys()), weights=list(durations.values()), k=1
+                )[0]
+                if current_state[0] == self.duration
+                else current_state[0]
+            )
+            self.change_duration(
+                key,
+                duration,
+                self.duration / 2 if random.random() < 0.3 else 0.0,
+            )
+
+        return self
+
+    def molding_rhythm(self, max_note: int = 3) -> ImageToMidi:
+        # min_bar: None | list[list[mid.Note]] = None
+        for index in range(int(len(self.track.messages.keys()))):
+            try:
+                bar = self.track.bar(index)
+            except Exception:
+                break
+            for msgs in bar:
+                if len(msgs) < max_note:
+                    continue
+                for msg in random.sample(msgs, k=len(msgs) - max_note):
+                    self.track.remove_msg(msg)
+
+        return self
 
 
 if __name__ == "__main__":
@@ -131,20 +200,27 @@ if __name__ == "__main__":
         data: list[list[str]] = json.load(f)
         molding_data = list(itertools.chain.from_iterable(data))
         model = markov.make_model(molding_data, order=4)
-        root = random.choice(["C", "D", "E", "F", "G", "A", "B"])
+        # root = random.choice(["C", "D", "E", "F", "G", "A", "B"])
         result = [
             elem
-            for elem in markov.make_result(model, max_items=50, seed="C", num=1)[0]
+            for elem in markov.make_result(model, max_items=50, seed="A", num=1)[0]
             if elem != "[EOF]"
-            and elem != "[BOF]"
-            and "on" not in elem
-            and "-" not in elem
+            and elem != "[BOF]"  # noqa
+            and "on" not in elem  # noqa
+            and "-" not in elem  # noqa
         ]
 
-        img = image.img_process(Image.open("image/text.png"))
+        img = image.img_process(Image.open("image/paint_line.png"))
         midi = mid.PrettyMIDI()
 
-        track = ImageToMidi(img, 0.5).molding_to_chords(result, bar_num=2)
+        track = (
+            ImageToMidi(img, 0.5)
+            .molding_to_chords(result, bar_num=2)
+            .molding_rhythm()
+            .molding_volume()
+            .molding_duration()
+            .get_track()
+        )
         midi.instruments.append(track.build_instrument())
         midi.write("dist/dist.mid")
         print(result)
